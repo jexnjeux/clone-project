546a94790d1e3ecc61879e3dd40c1fd6c67ae59e
JIEUN NA <68100358+jexnjeux@users.noreply.github.com>
2024-01-22 09:59:25 +0900

    style: [#3] 초기 스타일 세팅 (#4)

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from 'styled-components';
+
+const palette = {
+  background: '#fff',
+  black: '#111',
+  grey: '#767676',
+  lightGrey: '#d1d1d1',
+  white: '#fff',
+  red: '#f15151',
+};
+
+const font = {
+  xs: '12px',
+  sm: '16px',
+  md: '24px',
+  lg: '40px',
+};
+
+const theme: DefaultTheme = { palette, font };
+
+export default theme;
