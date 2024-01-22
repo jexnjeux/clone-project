5b0d47685d5f18fc9098f5ad54f56f675a38c005
JIEUN NA <68100358+jexnjeux@users.noreply.github.com>
2024-01-22 09:59:25 +0900

    style: [#3] 초기 스타일 세팅 (#4)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import GlobalStyles from './styles/globalstyles';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
     <App />
   </React.StrictMode>,
-)
+);
