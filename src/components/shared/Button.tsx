1864e00211f6cdd467c7af100bb075ea03c49fb5
JIEUN NA <68100358+jexnjeux@users.noreply.github.com>
2024-01-22 20:28:47 +0900

    feat: [#7] Button Component 구현, theme palette 색상 추가 및 수정 (#8)
    * feat: [#7] theme palette 색상 추가 및 변경
    
    * feat: [#7] Button Component 구현

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import { buttonColorMap, buttonSizeMap } from '../../styles/button';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  $solid?: boolean;
+  size?: 'sm' | 'md';
+}
+
+const Button = styled.button<ButtonProps>`
+  ${({ $solid }) => ($solid ? buttonColorMap.solid : buttonColorMap.outline)}
+  ${({ size = 'sm' }) => buttonSizeMap[size]}
+`;
+
+export default Button;
