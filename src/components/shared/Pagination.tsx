1f4cbb973b244bee7a3bb57772dd1b2a6f787ff1
jexnjeux <zieun.na@gmail.com>
2024-01-26 05:59:16 +0900

    feat: [#27] Main Page 페이지네이션

---
+++ src/components/shared/Pagination.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import usePagination from '../../hooks/usePagination';
+import Button from './Button';
+import ArrowRightIcon from '../../assets/icons/ArrowRightIcon';
+import ArrowLeftIcon from '../../assets/icons/ArrowLeftIcon';
+import { ELLIPSIS } from '../../constants/pagination';
+
+interface PaginationProps {
+  totalPage: number;
+  currentPage: number;
+  onChangePage: (page: number | string) => Promise<void>;
+  onClickArrow: (direction: 'left' | 'right') => Promise<void>;
+}
+
+function Pagination({
+  totalPage,
+  currentPage,
+  onChangePage,
+  onClickArrow,
+}: PaginationProps) {
+  const pages: (number | string)[] = usePagination(totalPage, currentPage);
+
+  return (
+    <Container>
+      <button
+        onClick={() => void onClickArrow('left')}
+        disabled={currentPage === 1}
+      >
+        <ArrowLeftIcon />
+      </button>
+      {pages.includes(ELLIPSIS) ? <ButtonWrap /> : null}
+      {pages.map((page: string | number, idx) => {
+        return currentPage === page ? (
+          <ButtonWrap key={idx}>
+            <Button $solid size="lg" onClick={() => void onChangePage(page)}>
+              <li>{page}</li>
+            </Button>
+          </ButtonWrap>
+        ) : (
+          <ButtonWrap key={idx}>
+            <StyledButton size="lg" onClick={() => void onChangePage(page)}>
+              {page}
+            </StyledButton>
+          </ButtonWrap>
+        );
+      })}
+      {pages.includes(ELLIPSIS) ? <ButtonWrap /> : null}
+      <button
+        onClick={() => void onClickArrow('right')}
+        disabled={currentPage === totalPage}
+      >
+        <ArrowRightIcon />
+      </button>
+    </Container>
+  );
+}
+
+export default Pagination;
+
+const Container = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+`;
+
+const ButtonWrap = styled.div`
+  width: 40px;
+`;
+
+const StyledButton = styled(Button)`
+  border: none;
+`;
