be45db254e10574a099cd3ccc33b9de354957a23
jexnjeux <zieun.na@gmail.com>
2024-01-27 12:47:22 +0900

    chore: [#44] @tanstack/react-query 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
