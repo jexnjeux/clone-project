bd31c4a536e29ab59b261a0e76b223f23cb3c641
JIEUN NA <68100358+jexnjeux@users.noreply.github.com>
2024-01-28 21:23:14 +0900

    style: [#77] Skeleton 반응형 적용 (#78)

--- src/components/shared/PhotoSkeleton.tsx
+++ src/components/shared/PhotoSkeleton.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import Skeleton from 'react-loading-skeleton';
+import { device } from '../../styles/theme';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 const PhotoSkeleton = () => {
   return (
     <Container>
-      <Skeleton height={200} highlightColor="#dbdbdb" />
+      <ResponsiveSkeleton highlightColor="#dbdbdb" />
     </Container>
   );
 };
@@ -14,4 +15,25 @@ export default PhotoSkeleton;
 
 const Container = styled.div`
   width: 200px;
+  height: 200px;
+
+  @media ${device.tablet} {
+    width: 400px;
+    height: 400px;
+  }
+
+  @media ${device.mobileL} {
+    width: 320px;
+    height: 320px;
+  }
+
+  @media ${device.mobileS} {
+    width: 300px;
+    height: 300px;
+  }
+`;
+
+const ResponsiveSkeleton = styled(Skeleton)`
+  width: 100%;
+  height: 100%;
 `;
