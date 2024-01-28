40728bed9340b3d4c67251650709a4e454728c82
jexnjeux <zieun.na@gmail.com>
2024-01-28 12:57:21 +0900

    refactor: [#58] 북마크 토글 컴포넌트 분리

---
+++ src/components/BookmarkToggle.tsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { PhotoItem } from '../types/photos';
+import useToggleBookmark from '../hooks/useToggleBookmark';
+import StyledHeartLineIcon from '../assets/icons/StyledHeartLineIcon';
+import StyledHeartFillIcon from '../assets/icons/StyledHeartFillIcon';
+import { device } from '../styles/theme';
+
+interface PhotoProps {
+  photo: PhotoItem;
+}
+
+function BookmarkToggle({ photo }: PhotoProps) {
+  const { handleToggleBookmark, getBookmarkStatus } = useToggleBookmark();
+
+  const handleBookmarkClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    handleToggleBookmark(photo);
+  };
+
+  return (
+    <IconButton
+      onClick={(e) => handleBookmarkClick(e)}
+      aria-label="Toggle bookmark"
+    >
+      {getBookmarkStatus(photo.id) ? (
+        <StyledHeartFillIcon colorname="red" />
+      ) : (
+        <StyledHeartLineIcon colorname="white" />
+      )}
+    </IconButton>
+  );
+}
+
+export default BookmarkToggle;
+
+const IconButton = styled.button`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  right: 4px;
+  bottom: 4px;
+
+  @media ${device.tablet} {
+    width: 32px;
+    height: 32px;
+    right: 8px;
+    bottom: 8px;
+  }
+`;
--- src/components/shared/Photo.tsx
+++ src/components/shared/Photo.tsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { PhotoItem } from '../../types/photos';
-import useToggleBookmark from '../../hooks/useToggleBookmark';
-import StyledHeartLineIcon from '../../assets/icons/StyledHeartLineIcon';
-import StyledHeartFillIcon from '../../assets/icons/StyledHeartFillIcon';
+import BookmarkToggle from '../BookmarkToggle';
 import { device } from '../../styles/theme';
 
 interface PhotoProps {
@@ -14,25 +12,10 @@ interface PhotoProps {
 }
 
 function Photo({ photo, url, alt, onClick, isSkeleton = false }: PhotoProps) {
-  const { handleToggleBookmark, getBookmarkStatus } = useToggleBookmark();
-
-  const handleBookmarkClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    handleToggleBookmark(photo);
-  };
-
   return (
     <Container onClick={onClick} $isSkeleton={isSkeleton}>
       <Thumbnail src={url} alt={alt} />
-      {!isSkeleton && (
-        <IconContainer onClick={(e) => handleBookmarkClick(e)}>
-          {getBookmarkStatus(photo.id) ? (
-            <StyledHeartFillIcon colorname="red" />
-          ) : (
-            <StyledHeartLineIcon colorname="white" />
-          )}
-        </IconContainer>
-      )}
+      {!isSkeleton && <BookmarkToggle photo={photo} />}
     </Container>
   );
 }
@@ -65,19 +48,4 @@ const Thumbnail = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-`;
-
-const IconContainer = styled.div`
-  position: absolute;
-  width: 20px;
-  height: 20px;
-  right: 4px;
-  bottom: 4px;
-
-  @media ${device.tablet} {
-    width: 32px;
-    height: 32px;
-    right: 8px;
-    bottom: 8px;
-  }
 `;
